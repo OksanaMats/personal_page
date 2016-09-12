6bcb9ff61beb3dd66b8f7fa17ba28683903908e1
oksanamatsevko <oksanavengrun@gmail.com>
2016-09-12 19:57:14 +0000

    final

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(function(){
-    $('#btn-send').click(sendForm);
+    $('#submit').click(sendForm);
 });
 function sendForm(e){
     e.preventDefault();
@@ -19,3 +19,59 @@ function sendForm(e){
         }
     });
 };
+$(window).scroll(function(){
+		/*Counter*/
+		$('.skills-row').not('.counted').each(function(){
+			if($(window).scrollTop()+$(window).height()>= $(this).offset().top){
+				$(this).addClass('counted').find('.timer').countTo();
+				
+				$(this).find('.skills-wrapper').not('.counted').each(function(){
+					var $progress = $(this).find('.progress')
+					var speed = parseInt($progress.attr("data-speed"),10);
+					var to = parseInt($progress.attr("data-to"),10);				
+					
+					to = 99 - to;
+					$(this).find('.progress').animate({top: to+"%"}, speed );					
+				});
+
+			} 
+		});		
+
+	});
+
+
+$(document).ready(function(){
+ 
+	// hide #back-top first
+	$("#back-top").hide();
+ 
+	// fade in #back-top
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				$('#back-top').fadeIn();
+			} else {
+				$('#back-top').fadeOut();
+			}
+		});
+ 
+		// scroll body to 0px on click
+		$('#back-top a').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	});
+ 
+ 
+  
+});
+
+$(document).on('click','.topmenu a', function(){
+	var el = $($(this).attr('href')),
+ 	elTop = el.offset().top;
+ 	 	$('body,html').animate({
+				scrollTop: elTop
+			}, 800);
+ });
---
+++ js/scroll.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#").on("click","a", function (event) {
+    	event.preventDefault();
+		var id  = $(this).attr('href'),
+		top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
+
+
